46231e26252c87f18c761735db88f23db3947ec3
MariemBelhadj <mariem.belhadj1309@gmail.com>
2022-09-30 08:03:58 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import myImage from './img/ma.jpg'
+class App extends React.Component{
+  constructor(props){
+    super(props)
+    this.state={
+      Person: {
+      fullName:"mariem belhadj",
+      bio:"aaaaaaaaaaaaaaaa",
+      profession:"aaaa",
+      imgSrc : myImage
+      },
+      show : false,
+      counter : 0
+    }
+  }
+ componentDidMount(){
+  this.myInterval = setInterval(()=>{
+    this.setState(prevState =>({
+      counter: prevState.counter+0.5
+    }))
+  },1000)
+ } 
+  handleShow = () => {
+    this.setState({
+      ...this.state.Person,
+      show: !this.state.show,
+    });
+  }
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  componnetWillUnmont(){
+    clearInterval(this.state.myInterval)
+    }
+    
+  
+      
+  
+  render() {
+    const counter = this.state.counter;
+    return (
+      <>
+       
+        {this.state.show && (
+          <>
+        <img src={this.state.Person.imgSrc} alt=""/>
+        <h1>{this.state.Person.fullName}</h1>
+        <h1>{this.state.Person.bio}</h1>
+        <h1>{this.state.Person.profession}</h1>
+        </>) }
+        <button onClick={this.handleShow}>click here</button>
+         <h1>count:{counter}</h1>
+      </>
+    )
+  }
 }
 
 export default App;
